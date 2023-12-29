14b48cc5bd6de21ea0a33eba5e0c4186ea17f96a
cgq20 <conanquinlivan@gmail.com>
2023-12-29 12:50:42 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-/* This is an example JavaScript file, feel free to remove/edit it anytime */
-console.log(
-  "%cProject by BigDevSoon",
-  'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
-);
-console.log("Check out more projects at https://bigdevsoon.me");
+// /* This is an example JavaScript file, feel free to remove/edit it anytime */
+// console.log(
+//   "%cProject by BigDevSoon",
+//   'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
+// );
+// console.log("Check out more projects at https://bigdevsoon.me");
+// console.log is used for print statements to appear in the console of the browser/browser dev tools
